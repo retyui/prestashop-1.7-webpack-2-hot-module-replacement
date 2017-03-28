5ffb8c759fd877e944f7004141f220b463e1b36f
DAVID♥ <vamnemne@gmail.com>
2017-03-28 09:16:46 +0300

    migration fixs

--- _dev/js/theme.js
+++ _dev/js/theme.js
@@ -22,7 +22,7 @@
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
-import 'expose?Tether!tether';
+import 'expose-loader?Tether!tether';
 import 'bootstrap/dist/js/bootstrap.min';
 import 'flexibility';
 import 'bootstrap-touchspin';
--- _dev/webpack.config.js
+++ _dev/webpack.config.js
@@ -1,85 +1,114 @@
 var ref;
 const HOT = ((ref = module.parent.filename) != null ? ref.indexOf('hot.webpack.js') : void 0) !== -1;
-console.log('Webpack HOT : ',HOT,'\n');
+console.log('Webpack HOT : ', HOT, '\n');
 
 var webpack = require('webpack');
 var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var plugins = [];
 
 var production = false;
 
 if (production) {
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    })
-  );
+	plugins.push(
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false
+			}
+		})
+	);
 }
 if (HOT) {
-  plugins.push(new webpack.HotModuleReplacementPlugin())
+	plugins.push(new webpack.HotModuleReplacementPlugin())
 }
 
 plugins.push(
-  new ExtractTextPlugin(
-    path.join('..', 'css', 'theme.css')
-    ,{
-      disable: HOT // if hot enabled disable ExtractTextPlugin
-    }
-  )
+	new ExtractTextPlugin({
+		filename: path.join('..', 'css', 'theme.css')
+		, allChunks: true
+		, disable: HOT // if hot enabled disable ExtractTextPlugin
+	})
 );
 
 let addHOT = (arr, disable) => {
-  if (HOT) {
-    arr.unshift('webpack/hot/dev-server', 'webpack-hot-middleware/client');
-  }
-  return arr;
+	if (HOT) {
+		arr.unshift('webpack/hot/dev-server', 'webpack-hot-middleware/client');
+	}
+	return arr;
 };
 
 module.exports = {
-  entry: {
-    theme: addHOT(['./js/theme.js'])
-  },
-  output: {
-    path: path.resolve(__dirname + '/../assets/js'),
-    filename: 'theme.js'
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loaders: (HOT? ['monkey-hot-loader','babel-loader']: ['babel-loader'])
-    }, {
-      test: /\.scss$/,
-      loader: ExtractTextPlugin.extract(
-        "style",
-        "css?sourceMap!postcss!sass?sourceMap"
-      )
-    }, {
-      test: /.(png|woff(2)?|eot|ttf|svg|jpe?g)(\?[a-z0-9=\.]+)?$/,
-      loader: 'file-loader?name=../css/[hash].[ext]'
-    }, {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract(
-        "style",
-        "css?sourceMap!postcss"
-      )
-    }]
-  },
-  postcss: function() {
-    return [require('postcss-flexibility')];
-  },
-  externals: {
-    prestashop: 'prestashop',
-    $: '$',
-    jquery: 'jQuery'
-  },
-  devtool: HOT ? 'cheap-module-inline-source-map' : 'source-map',
-  plugins: plugins,
-  resolve: {
-    extensions: ['', '.js', '.scss']
-  }
+	entry: {
+		theme: addHOT(['./js/theme.js'])
+	}
+	, output: {
+		path: path.resolve(__dirname + '/../assets/js')
+		, filename: 'theme.js'
+	}
+	, module: {
+		rules: [{
+			test: /\.js$/
+			, use: {
+				loader: 'babel-loader'
+				, options: {
+					presets: [
+						['env', {
+							modules: false
+								// ,targets: {
+								//   browsers: ['last 2 versions', 'safari 7']
+								// }
+						}]
+					]
+					, plugins: ['syntax-dynamic-import']
+				}
+			}
+		}, {
+			test: /\.scss$/
+			, loader: ExtractTextPlugin.extract({
+				fallback: 'style-loader'
+				, use: [{
+					loader: 'css-loader'
+					, options: {
+						sourceMap: true
+						, importLoaders: 2
+					}
+				}, {
+					loader: 'postcss-loader'
+				}, {
+					loader: 'sass-loader'
+					, options: {
+						sourceMap: true
+					}
+				}]
+			})
+		}, {
+			test: /.(png|woff(2)?|eot|ttf|svg|jpe?g)(\?[a-z0-9=\.]+)?$/
+			, loader: 'file-loader?name=../css/[hash].[ext]'
+		}, {
+			test: /\.css$/
+			, loader: ExtractTextPlugin.extract({
+				fallback: 'style-loader'
+				, use: [{
+					loader: 'css-loader'
+					, options: {
+						sourceMap: true
+						, importLoaders: 1
+					}
+				}, {
+					loader: 'postcss-loader'
+				}]
+			})
+		}]
+	}
+	, externals: {
+		prestashop: 'prestashop'
+		, $: '$'
+		, jquery: 'jQuery'
+	}
+	, devtool: HOT ? 'cheap-module-inline-source-map' : 'source-map'
+	, plugins: plugins
+	, resolve: {
+		extensions: ['.js', '.scss']
+	}
 };
